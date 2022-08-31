acc47a158cdc74ee945784a6f634a12a801dbd4b
akramen <89003315+akramen@users.noreply.github.com>
2022-08-31 20:51:40 +0900

    Update index.js
    36.0の.0が表示されるように改善

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const yourTmpIs = document.getElementById('yourTmpIs')
 Button.onclick = () => {
 let Tmp = [36.7,36.6,36.5,36.4, 36.3, 36.2, 36.1, 36.0, 35.9, 35.8]
 let ResultTmp = Tmp[Math.floor(Math.random() * Tmp.length)];
-console.log(ResultTmp);
+console.log(ResultTmp.toFixed(1));
 
 
 yourTmpIs.innerText = 'あなたの今の体温';
 const header = document.createElement('h2');
 resultarea2.innerText = '';
-header.innerText = ResultTmp;
+header.innerText = ResultTmp.toFixed(1);
 resultarea2.appendChild(header);
 }
 
